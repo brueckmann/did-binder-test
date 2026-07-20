8adad217b985ccb1cd026a9ff7dd59f9571602d4
Ghd <gracia.brueckmann@unibe.ch>
2026-07-20 19:24:26 +0200

    update

--- 02_rscripts/install.R
+++ 02_rscripts/install.R
@@ -1,13 +1,24 @@
 ### install packages
-
-
-packs <- c('dataverse', 'causaldata', 'data.table', 'estimatr', 'evaluate', 'fixest', 'ggplot2',
-           'haven', 
-            'magrittr', 'modelsummary', 'openssl', 'ragg',
-           'softbib', 'stringr', 'this.path', 'tinytable', 'xfun'
-           )
+packs <- c(
+  'dataverse',
+  'causaldata',
+  'data.table',
+  'estimatr',
+  'evaluate',
+  'fixest',
+  'ggplot2',
+  'haven',
+  'magrittr',
+  'modelsummary',
+  'openssl',
+  'ragg',
+  'softbib',
+  'stringr',
+  'this.path',
+  'tinytable',
+  'xfun'
+)
 
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/2026-07-17"))
 
 install.packages(packs)
-
