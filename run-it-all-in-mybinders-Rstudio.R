c01b44b0b34337c450f9d51652e47706cec5cec3
Ghd <gracia.brueckmann@unibe.ch>
2026-07-21 01:18:36 +0200

    As `bash run.sh` will use too much memory (more than 2 GB) when performed on [mybinder](https://2i2c.mybinder.org/v2/gh/brueckmann/did-binder-test/HEAD), I suggest using this instead on the GUI.

---
+++ run-it-all-in-mybinders-Rstudio.R
@@ -0,0 +1,11 @@
+# run-it-all-in-mybinders-Rstudio.R
+
+# when you are using https://hub.2i2c.mybinder.org/user/brueckmann-did-binder-test-6htfzh5r/rstudio/
+
+source("install.R")
+
+source("02_rscripts/00_get_data.R")
+source("02_rscripts/01_data_cleaning.R")
+source("02_rscripts/02_descriptives.R")
+source("02_rscripts/03_modeling.R")
+source("02_rscripts/98_sessioninfo_and_cite.R")
