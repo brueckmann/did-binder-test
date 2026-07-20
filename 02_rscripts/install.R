6dc7ff8b2449539a4cebdbcc416dd76f4b6dd955
Ghd <gracia.brueckmann@unibe.ch>
2026-07-20 16:08:16 +0200

    trying the full pipe

--- 02_rscripts/install.R
+++ 02_rscripts/install.R
@@ -3,7 +3,7 @@
 
 packs <- c('dataverse', 'causaldata', 'data.table', 'estimatr', 'evaluate', 'fixest', 'ggplot2',
            'haven', 
-           #'lfe', #Package which is only available in source form, and may need compilation of C/C++/Fortran: ‘lfe’
+           'lfe',
             'magrittr', 'modelsummary', 'openssl', 'ragg',
            'softbib', 'stringr', 'this.path', 'tinytable', 'xfun' )
 
