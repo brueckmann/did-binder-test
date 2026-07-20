d5080300a0a8cdeacbaf6a65d4ee29df3ca0621e
Ghd <gracia.brueckmann@unibe.ch>
2026-07-20 15:50:20 +0200

    initial commit

---
+++ 02_rscripts/01_data_cleaning.R
@@ -0,0 +1,184 @@
+#### load packages ####
+library(tidyverse)
+
+
+### Import data ####
+
+# Note current script location
+current_script_dir <- this.path::this.dir()
+cat("Script directory:", current_script_dir, "\n")
+
+# Set project root
+project_root <- this.path::this.proj() 
+setwd(project_root)
+cat("Project root set to:", getwd(), "\n")
+
+
+
+### Import data ####
+
+# load data
+Replication_data <- haven::read_dta(file.path(project_root, "01_data/raw/Replication_Dataset.dta"))
+
+### Data cleaning of important variables ####
+
+####  Car Emission Groups
+
+Replication_data <- Replication_data %>%
+  mutate(
+    groups = case_when(
+      dummy_euro_4 == 1 & dummy_diesel == 1 ~ "Diesel Euro 4",
+      dummy_euro_5 == 1 & dummy_diesel == 1 ~ "Diesel Euro 5",
+      dummy_euro_4 == 1 & dummy_petrol == 1 ~ "Petrol Euro 4",
+      dummy_euro_5 == 1 & dummy_petrol == 1 ~ "Petrol Euro 5"
+    )
+  )
+
+
+### Education
+
+Replication_data <- Replication_data %>%
+  mutate(
+    education = case_when(
+      education_level_it_original < 7 ~ 1,
+      education_level_it_original == 7 |
+        education_level_it_original == 9 ~ 2,
+      education_level_it_original == 8 |
+        education_level_it_original == 10 |
+        education_level_it_original == 11 |
+        education_level_it_original == 12 ~ 3,
+      education_level_it_original == 13 |
+        education_level_it_original == 14 ~ 4
+    )
+  )
+
+Replication_data$education_fac <- factor(
+  Replication_data$education,
+  levels = c(1, 2, 3, 4),
+  labels = c("High school diploma", "Bachelors", "MA or higher", "Unknown")
+)
+
+Replication_data$EDU <- factor(Replication_data$education_fac)
+
+#### Income
+
+Replication_data$INC <- factor(Replication_data$profile_gross_personal_eu)
+
+Replication_data <- Replication_data %>%
+  mutate(
+    profile_gross_personal_eu_2 = case_when(
+      profile_gross_personal_eu == 1 |
+        profile_gross_personal_eu == 2 |
+        profile_gross_personal_eu == 3 ~ 1,
+      profile_gross_personal_eu ==
+        4 |
+        profile_gross_personal_eu == 5 |
+        profile_gross_personal_eu == 6 ~ 2,
+      profile_gross_personal_eu ==
+        7 |
+        profile_gross_personal_eu == 8 |
+        profile_gross_personal_eu == 9 ~ 3,
+      profile_gross_personal_eu ==
+        10 | profile_gross_personal_eu == 11 |
+        profile_gross_personal_eu == 12 ~ 4,
+      profile_gross_personal_eu ==
+        13 | profile_gross_personal_eu == 14 ~ 5,
+      profile_gross_personal_eu ==
+        98 | profile_gross_personal_eu == 99 ~ 6
+    )
+  )
+
+Replication_data$profile_gross_personal_eu_2_fac <- factor(
+  Replication_data$profile_gross_personal_eu_2,
+  levels = c(1, 2, 3, 4, 5, 6),
+  labels = c(
+    "Less than 14.999 EUR per year",
+    "From 15.000 EUR to 29.999 EUR per year",
+    "From 30.000 EUR to 44.999 EUR per year",
+    "From 45.000 EUR 69.999 EUR per year",
+    "From 70.000 EUR and more",
+    "No Answer / DK"
+  )
+)
+
+Replication_data$income_fac <- Replication_data$profile_gross_personal_eu_2_fac
+
+#### Age
+
+Replication_data <- Replication_data %>%
+  mutate(
+    age_rc = case_when(
+      age == 18 |
+        age == 19 | age == 20 | age == 21 | age == 22 |
+        age == 23 | age == 24 ~ 1,
+      age == 25 |
+        age == 26 |
+        age == 27 |
+        age == 28 |
+        age == 29 | age == 30 | age == 31 | age == 32 |
+        age == 33 | age == 34 ~ 2,
+      age == 35 |
+        age == 36 |
+        age == 37 |
+        age == 38 |
+        age == 39 | age == 40 | age == 41 | age == 42 |
+        age == 43 | age == 44 ~ 3,
+      age == 45 |
+        age == 46 |
+        age == 47 |
+        age == 48 |
+        age == 49 | age == 50 | age == 51 | age == 52 |
+        age == 53 | age == 54 ~ 4,
+      age == 55 | age > 55 ~ 5
+    )
+  )
+
+Replication_data$age_fac <- factor(
+  Replication_data$age_rc,
+  levels = c(1, 2, 3, 4, 5),
+  labels = c("18-24", "25-34", "35-44", "45-54", "55+")
+)
+
+#### Gender
+Replication_data <- Replication_data %>%
+  mutate(female_fac = case_when(female == 1 ~ "Female", female == 0 ~ "Male"))
+
+Replication_data$female_fac <- factor(
+  Replication_data$female_fac,
+  levels = c("Male", "Female"),
+  labels = c("Male", "Female")
+)
+
+
+### Area_B_costs
+
+
+Replication_data$diesel_euro4_fac <- factor(Replication_data$diesel_euro4)
+Replication_data$area_b_cost_fact <- factor(
+  Replication_data$cost_area_b,
+  levels = c(1, 2, 3, 4, 5, 6, 7, 8),
+  labels = c(
+    "No cost",
+    "Less than EUR 500",
+    "EUR 500 - EUR 1,500",
+    "EUR 1,500  - EUR 2,500",
+    "EUR 2,500 - EUR 5,000",
+    "EUR 5,000 - EUR 10,000",
+    "Above EUR 10,000",
+    "Don't know"
+  )
+)
+
+
+
+
+# remove all haven labels.
+data <- Replication_data |> haven::zap_label()  |> haven::zap_labels()
+
+# Define the processed data directory using this.path
+data_dir <- this.path::path.join(project_root, "01_data", "processed")
+# Create directory if it doesn't exist
+dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
+
+save(data, file =
+this.path::path.join(data_dir, "data.Rdata"))
---
+++ 02_rscripts/02_descriptives.R
@@ -0,0 +1,192 @@
+#### load packages ####
+library(tidyverse)
+
+
+### Import data ####
+
+# Note current script location
+current_script_dir <- this.path::this.dir()
+cat("Script directory:", current_script_dir, "\n")
+
+# Set project root
+project_root <- this.path::this.proj() 
+setwd(project_root)
+cat("Project root set to:", getwd(), "\n")
+
+
+# load data
+load(file.path(project_root, "01_data/processed/data.Rdata"))
+
+
+# Define the table directory using this.path
+tab_dir <- this.path::path.join(project_root, "03_output", "tables")
+# Create directory if it doesn't exist
+dir.create(tab_dir, showWarnings = FALSE, recursive = TRUE)
+
+
+### To produce Table 1 in the Paper ####
+
+# Age
+
+age_tbl <- data |>
+  count(age_fac, groups, name = "n") |>
+  pivot_wider(names_from  = groups,
+              values_from = n,
+              values_fill = 0) |>
+  rowwise(age_fac) |>
+  mutate(full_sample = sum(c_across(everything()))) |>
+  ungroup()  |>
+  dplyr::relocate("full_sample") |> ### move to second position
+  dplyr::relocate("age_fac") |> ### move to 1st column
+  select(!contains("NA")) |>   ### remove NA column
+  mutate(across((2:6), ~ (.x / sum(.x)) * 100)) ### make everything percent
+
+age_tbl <- tinytable::tt(age_tbl ,
+                         width = c(.5, .1, .1, .1, .1, .1) ,
+                         digits = 2)
+
+# Gender
+
+gender_tbl <- data |>
+  count(female_fac, groups, name = "n") |>
+  pivot_wider(names_from  = groups,
+              values_from = n,
+              values_fill = 0) |>
+  rowwise(female_fac) |>
+  mutate(full_sample = sum(c_across(everything()))) |>
+  ungroup()  |>
+  dplyr::relocate("full_sample") |> ### move to second position
+  dplyr::relocate("female_fac") |> ### move to 1st column
+  select(!contains("NA")) |>   ### remove NA column
+  mutate(across((2:6), ~ (.x / sum(.x)) * 100)) ### make everything percent
+
+gender_tbl <- tinytable::tt(gender_tbl ,
+                            width = c(.5, .1, .1, .1, .1, .1) ,
+                            digits = 2)
+
+# gender_table <- table(data$female_fac, data$groups)
+# full_sample <- table(data$female_fac)
+#
+# gender_combined <- cbind(full_sample, gender_table)
+#
+# # Make Colwise to 100%
+#
+# N <- colSums(gender_combined)
+# gender_combined <- t(apply(gender_combined, MAR = 1,
+#                            function (x) (x / N) * 100))
+# gender_combined <- rbind(round(gender_combined,1),
+#                          N)
+#
+
+# Education
+
+educ_tbl <- data |>
+  count(education_fac, groups, name = "n") |>
+  pivot_wider(names_from  = groups,
+              values_from = n,
+              values_fill = 0) |>
+  rowwise(education_fac) |>
+  mutate(full_sample = sum(c_across(everything()))) |>
+  ungroup()  |>
+  dplyr::relocate("full_sample") |> ### move to second position
+  dplyr::relocate("education_fac") |> ### move to 1st column
+  select(!contains("NA")) |>   ### remove NA column
+  mutate(across((2:6), ~ (.x / sum(.x)) * 100)) ### make everything percent
+
+educ_tbl <- tinytable::tt(educ_tbl ,
+                          width = c(.5, .1, .1, .1, .1, .1) ,
+                          digits = 2)
+
+
+# education_table <- table(data$education_fac, data$groups)
+# full_sample <- table(data$education_fac)
+#
+# education_combined <- cbind(full_sample, education_table)
+#
+#
+# # Make Colwise to 100%
+#
+# N <- colSums(education_combined)
+# education_combined <- t(apply(education_combined, MAR = 1,
+#                               function (x) (x / N) * 100))
+# education_combined <- rbind(round(education_combined,1),
+#                             N)
+
+
+
+
+# Income
+
+income_tbl <- data |>
+  count(income_fac, groups, name = "n") |>
+  pivot_wider(names_from  = groups,
+              values_from = n,
+              values_fill = 0) |>
+  rowwise(income_fac) |>
+  mutate(full_sample = sum(c_across(everything()))) |>
+  ungroup()  |>
+  dplyr::relocate("full_sample") |> ### move to second position
+  dplyr::relocate("income_fac") |> ### move to 1st column
+  select(!contains("NA")) |>   ### remove NA column
+  mutate(across((2:6), ~ (.x / sum(.x)) * 100)) ### make everything percent
+
+income_tbl <- tinytable::tt(income_tbl,
+                            width = c(.5, .1, .1, .1, .1, .1) ,
+                            digits = 2)
+
+# Obs per group
+
+n_tbl <- data |>
+  count(income_fac, groups, name = "n") |>
+  pivot_wider(names_from  = groups,
+              values_from = n,
+              values_fill = 0) |>
+  rowwise(income_fac) |>
+  mutate(full_sample = sum(c_across(everything()))) |>
+  ungroup()  |>
+  dplyr::relocate("full_sample") |> ### move to second position
+  dplyr::relocate("income_fac") |> ### move to 1st column
+  select(!contains("NA")) |>   ### remove NA column
+  summarise(across(where(is.numeric), \(x) sum(x, na.rm = TRUE))) |> ### Make just the sum
+  mutate(N = "N") |>
+  dplyr::relocate("N") ### move N to 1st column
+
+n_tbl <- tinytable::tt(n_tbl  ,
+                       width = c(.5, .1, .1, .1, .1, .1) ,
+                       digits = 2)
+
+#
+# income_table <- table(data$profile_gross_personal_eu_2_fac, data$groups)
+#
+# full_sample <- table(data$profile_gross_personal_eu_2_fac)
+#
+# income_combined <- cbind(full_sample, income_table)
+#
+#
+# # Make Colwise to 100%
+#
+# N <- colSums(income_combined)
+# income_combined <- t(apply(income_combined, MAR = 1,
+#                            function (x) (x / N) * 100))
+# income_combined <- rbind(round(income_combined,1),
+#                          N)
+
+
+# Combine to generate Table 1 in the paper
+
+for_tab_1 <- rbind2(age_tbl, gender_tbl, headers = FALSE, use_names = FALSE)
+for_tab_1 <- rbind2(for_tab_1, educ_tbl, headers = FALSE, use_names = FALSE)
+for_tab_1 <- rbind2(for_tab_1,
+                    income_tbl,
+                    headers = FALSE,
+                    use_names = FALSE)
+tab_1 <- rbind2(for_tab_1, n_tbl, headers = FALSE, use_names = FALSE)
+
+tab_1 |> tinytable::save_tt( this.path::path.join(tab_dir, "table1.md"), overwrite = TRUE)
+
+
+rm(age_tbl, gender_tbl, educ_tbl , income_tbl, n_tbl, for_tab_1)
+
+
+
+
